0a262964ae58f38cb8be6e795ed53b87ec57f53f
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-10-19 22:57:48 +0300

    add implementation of lab-4

---
+++ lab-4/lpm_or1_inst.vhd
@@ -0,0 +1,9 @@
+lpm_or1_inst : lpm_or1 PORT MAP (
+		data0x	 => data0x_sig,
+		data1x	 => data1x_sig,
+		data2x	 => data2x_sig,
+		data3x	 => data3x_sig,
+		data4x	 => data4x_sig,
+		data5x	 => data5x_sig,
+		result	 => result_sig
+	);
